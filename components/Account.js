104fce35b2dfcdbcd4c1fa6ef3974db2622b8d8b
kitutune <p.kitutune@gmail.com>
2022-04-10 21:49:54 +0900

    ✨ Create an upload widget#

--- components/Account.js
+++ components/Account.js
@@ -1,3 +1,5 @@
+// Import the new component
+import Avatar from './Avatar'
 import { useState, useEffect } from 'react'
 import { supabase } from '../utils/supabaseClient'
 
@@ -67,6 +69,16 @@ export default function Account({ session }) {
 
   return (
     <div className="form-widget">
+         {/* Add to the body */}
+    <Avatar
+      url={avatar_url}
+      size={150}
+      onUpload={(url) => {
+        setAvatarUrl(url)
+        updateProfile({ username, website, avatar_url: url })
+      }}
+    />
+    {/* ... */}
       <div>
         <label htmlFor="email">Email</label>
         <input id="email" type="text" value={session.user.email} disabled />
